b7f0b749a6aff90aaa1902d5275208f6a79f546e
satishkona7 <konasatish44@gmail.com>
2024-06-19 15:12:20 +0530

    v4

--- main.tf
+++ main.tf
@@ -14,10 +14,11 @@ resource "azurerm_resource_group" "main" {
     name = "main"
     location = "east us"
 }
-resource "azurerm_service_plan" "mainsvc" {
-    name = "mainsvc"
-    resource_group_name = azurerm_resource_group.main.name
-    location = azurerm_resource_group.main.location
-    os_type = "Linux"
-    sku_name = "S1"
+resource "azurerm_resource_group" "aaaaa" {
+    name = "aaaaa"
+    location = "east us"
+}
+resource "azurerm_resource_group" "1111" {
+    name = "1111"
+    location = "east us"
 }
